f69e4d7b27d09f0f05f069ff35435ebba55c2906
Nithai Karingula <nithai@Nithais-MacBook-Pro.local>
2022-08-18 15:11:52 -0700

    update

--- AWS/ansible_setup.sh
+++ AWS/ansible_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt update -y 
 sudo apt upgrade -y 
-sudo apt install -y git unzip ansible python3 python3-boto3 python3-boto
+sudo apt install -y git unzip python3 python3-boto3 python3-boto python3-pip
+sudo yes | pip install ansible
